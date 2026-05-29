7c3c33325650c7ffdfb8328143f7b96053340c17
Your Name <garimasingh44443@gmail.com>
2026-05-29 23:55:35 +0530

    fixed tsparticles

--- src/components/ParticleBackground.jsx
+++ src/components/ParticleBackground.jsx
@@ -1,35 +1,51 @@
-export default function ParticleBackground() {
-  return (
-    <>
-      <div
-        style={{
-          position: "fixed",
-          width: "300px",
-          height: "300px",
-          background: "cyan",
-          borderRadius: "50%",
-          filter: "blur(120px)",
-          top: "10%",
-          left: "10%",
-          opacity: 0.3,
-          zIndex: -1,
-        }}
-      />
+import { useMemo } from "react";
+import Particles from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
 
-      <div
-        style={{
-          position: "fixed",
-          width: "300px",
-          height: "300px",
-          background: "purple",
-          borderRadius: "50%",
-          filter: "blur(120px)",
-          bottom: "10%",
-          right: "10%",
-          opacity: 0.3,
-          zIndex: -1,
-        }}
-      />
-    </>
+export default function ParticleBackground() {
+
+  const options = useMemo(() => ({
+    background: {
+      color: {
+        value: "#050816",
+      },
+    },
+
+    particles: {
+      number: {
+        value: 60,
+      },
+
+      color: {
+        value: "#00ffff",
+      },
+
+      links: {
+        enable: true,
+        color: "#00ffff",
+      },
+
+      move: {
+        enable: true,
+        speed: 1,
+      },
+
+      opacity: {
+        value: 0.5,
+      },
+
+      size: {
+        value: 3,
+      },
+    },
+  }), []);
+
+  return (
+    <Particles
+      id="tsparticles"
+      init={loadSlim}
+      options={options}
+      className="absolute inset-0 -z-10"
+    />
   );
 }
